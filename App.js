224a9dc2f3d694d6dc98bea127912ec5ba4fbece
JuanFlores <juanflz7777@gmail.com>
2022-07-07 09:32:28 -0700

    added console log message for debuging and commented out

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-// View -> UIView
 
+// View -> UIView
 export default function App() {
+  // console.log("App executed");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
