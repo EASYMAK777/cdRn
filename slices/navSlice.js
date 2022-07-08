0842953868c6eab3e3d6a97af3e744a287a44e67
JuanFlores <juanflz7777@gmail.com>
2022-07-08 07:45:24 -0700

    exporting navSlice and createing a function to create a navslice

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+
+
+// App initial State, initial values are 0, or null when app is started.
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+export const navSlice = createSlice({
+    
+})
