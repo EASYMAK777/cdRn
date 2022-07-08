ed02950bee2274898f5e3ee2c340929f0cdb97d3
JuanFlores <juanflz7777@gmail.com>
2022-07-08 12:49:23 -0700

    installed react native elements, imported and added safeAreaProvider componenent to entire App

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 // 1. Set up Redux
 
@@ -15,7 +16,10 @@ export default function App() {
   return (
    
     <Provider store={store}>
+      <SafeAreaProvider>
+
        <HomeScreen/>
+      </SafeAreaProvider>
     </Provider>
       
 
