ddfaf10c871575d31c89afcd7adab6f3f498fe64
JuanFlores <juanflz7777@gmail.com>
2022-07-09 01:15:49 -0700

    changed arrow icon color

--- components/navOptions.js
+++ components/navOptions.js
@@ -37,7 +37,7 @@ const NavOptions = () => {
         <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
         <View>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-            <Icon name="arrowright" color="white" type='antdesign'/>
+            <Icon style={tw`mt-2 text-lg font-semibold`} name="arrowright" color="green" type='antdesign'/>
         </View>
         </TouchableOpacity>
 
