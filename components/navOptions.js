1bbdc6c5d706807ef2d1c5ca9ae42a5288fa3129
JuanFlores <juanflz7777@gmail.com>
2022-07-09 01:17:54 -0700

    changed arrow icon bg color

--- components/navOptions.js
+++ components/navOptions.js
@@ -37,7 +37,7 @@ const NavOptions = () => {
         <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
         <View>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-            <Icon style={tw`mt-2 text-lg font-semibold`} name="arrowright" color="green" type='antdesign'/>
+            <Icon style={tw`p-2 bg-black rounded-full w-10 mt-3`} name="arrowright" color="green" type='antdesign'/>
         </View>
         </TouchableOpacity>
 
