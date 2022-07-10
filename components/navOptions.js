3f8d0d05884485cacaa207c3ef0b865315ef70e2
JuanFlores <juanflz7777@gmail.com>
2022-07-09 18:36:15 -0700

    used onPress function to take user to mapscreen

--- components/navOptions.js
+++ components/navOptions.js
@@ -1,4 +1,5 @@
 
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
 import { FlatList, TouchableOpacity, Text, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
@@ -29,6 +30,7 @@ const data = [{
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
   return (
 
 
@@ -36,7 +38,9 @@ const NavOptions = () => {
         data={data}
        keyExtractor={(item) => item.id}
        renderItem={({ item }) =>(
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+         style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
         <View>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             {/* <Icon style={tw`p-2 bg-black rounded-full w-10 mt-3`} name="arrowright" color="green" type='antdesign'/> */}
