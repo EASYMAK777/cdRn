e17c52c6b1183508b48987c8edac9245829cfa91
JuanFlores <juanflz7777@gmail.com>
2022-07-11 13:14:44 -0700

    imported google places auto complete to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,17 +2,17 @@ import React from "react";
 import { SafeAreaView, View, Text } from "react-native";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView style={tw`bg-white h-full`}>
-            <Text style={tw`text-red-500 p-10`}>The Charlotte Driver Logo Here</Text>
-            <View style={tw`p-5`}>
-                <NavOptions />
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <Text style={tw`text-red-500 p-10`}>The Charlotte Driver Logo Here</Text>
+      <View style={tw`p-5`}>
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
-
