31fd3d25b0fcb88ddae1952e1ebf80d28e03ffd2
JuanFlores <juanflz7777@gmail.com>
2022-07-12 09:04:33 -0700

    created funtion using the react redux dispatch feature to send user location origin information to a data layer that can be recalled anywhere on app,

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,9 +5,14 @@ import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "./slices/navSlice";
 
 // HOMESCREEN COMPONENT IS FUNTIONAL COMPONENT
 const HomeScreen = () => {
+  // USING REACT REDUX DISPATCH FUNCTION--->REFER TO LINE 38-40 AT THE ONPRESS EVENT
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       {/* THIS IS THE PLACE WHERE THE LOGO SHOULD GO */}
@@ -29,8 +34,19 @@ const HomeScreen = () => {
           onPress={(data, details = null) => {
             console.log(data);
             console.log(details);
+            // SENDS INFORMATION FROM THE OBJECT TO THE "DATA LAYER" USING REDUX
+            dispatch(
+              // APP SAVES ORIGIN LOCATION INFORMATION IN REDUX "DATA LAYER"
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
           }}
+          // GETS DETAILS ABOUT USERS LOCATION. CITY STATE COUNTY LONG LAT ETC
           fetchDetails={true}
+          // PRESSING RETURN KEY CAN ALSO START SEARCH
+          returnKeyType={"search"}
           // CLIENT MUST HAVE AT LEAST ENTERED TWO DIGITS UNTIL AUTOCOMPLETE STARTS
           minLength={2}
           // REMOVES "POWERED BY GOOGLE" LOGO
