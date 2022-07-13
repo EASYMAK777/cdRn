67b95783a0c458036ebed017e913b05d79d5538b
JuanFlores <juanflz7777@gmail.com>
2022-07-13 09:22:54 -0700

    added and styled two view comp imported tw css into doc

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
 
 const MapScreen = () => {
   return (
     <View>
       <Text>This is the Map Screen</Text>
+      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}></View>
     </View>
-  )
-}
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
