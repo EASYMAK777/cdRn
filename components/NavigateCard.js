97f87bf0c47518c1577818deedc00104d6e805fa
JuanFlores <juanflz7777@gmail.com>
2022-07-13 12:53:06 -0700

    importing googloeplacesautocomplete and api key so user can put dest address

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
   return (
@@ -8,7 +10,13 @@ const NavigateCard = () => {
       <View>
         <Text style={tw`text-center py-5 text-xl`}>Good Morning, User</Text>
         <View style={tw`border-t border-gray-200 flex-shrink`}>
-          <View></View>
+          <View>
+            <GooglePlacesAutocomplete
+              placeholder="Where to?"
+              nearbyPlacesAPI="GooglePlacesSearch"
+              debounce={400}
+            />
+          </View>
         </View>
       </View>
     </SafeAreaView>
