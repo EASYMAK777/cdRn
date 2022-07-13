4bce83145b3e4918140adf40bc5bd44b3e76bf19
JuanFlores <juanflz7777@gmail.com>
2022-07-13 09:49:05 -0700

    added comments

--- components/Map.js
+++ components/Map.js
@@ -6,11 +6,16 @@ import { selectOrigin } from "../slices/navSlice";
 import { useSelector } from "react-redux";
 
 const Map = () => {
+  // USING REACT REDUX TO PULL FROM DATALAYER FROM STORE JS TO SHOW MAP OF USERS LOCATION
   const origin = useSelector(selectOrigin);
   return (
+    // MAPVEW COMPONENET FROM  REACT-NATIVVE-MAPS DEPENDENCU
     <MapView
+      // STYLING CONTAINER SO MAP CAN RENDER
       style={tw`flex-1`}
+      //   MUTED STANDARD TYPE MAP REMOVES ALL IRRELEVANT ICONS
       mapType="mutedStandard"
+      //   USES GOOGLE LOCATION API INFORMATION STORED IN STORE.JS FILE TO PULL FROM DATALAYER USING REACT REDUX RECUDUERS AND USESELECTOR FUNCTIONS TO DISPLAY USERS ORIGIN LONG AND LAT ON A MAP
       initialRegion={{
         latitude: origin.location.lat,
         longitude: origin.location.lng,
