5108146e414fe4be3e3f4e5ea6618725b66530cc
JuanFlores <juanflz7777@gmail.com>
2022-07-13 12:27:14 -0700

    fixed an import issue

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,9 +4,10 @@ import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import MapView from "react-native-maps";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigateCard } from "../components/NavigateCard";
 
 const MapScreen = () => {
-  const stack = createStackNavigator();
+  const stack = createNativeStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
@@ -15,15 +16,15 @@ const MapScreen = () => {
       </View>
 
       <View style={tw`h-1/2`}>
-        <Stack.Navigator>
-          <Stack.Screen
+        <stack.Navigator>
+          <stack.Screen
             name="NavigateCard"
             component={NavigateCard}
             options={{
               headerShown: false,
             }}
           />
-        </Stack.Navigator>
+        </stack.Navigator>
       </View>
     </View>
   );
