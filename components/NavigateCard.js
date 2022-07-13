a2575380dfbe098e36ee3ea56a65d63a81bb9d00
JuanFlores <juanflz7777@gmail.com>
2022-07-13 13:12:30 -0700

    Using hooks for dispatching infro from datalayer with onpress function

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,18 +3,31 @@ import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
 
 const NavigateCard = () => {
+  // USING HOOK TO REACH DISPATCH OBJECT
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <View>
         <Text style={tw`text-center py-5 text-xl`}>Good Morning, User</Text>
         <View style={tw`border-t border-gray-200 flex-shrink`}>
           <View>
+            {/* GOOGLE PLACES COMPONENT REFER TO HOMESCREEN.JS FOR ELABORATION*/}
             <GooglePlacesAutocomplete
               placeholder="Where to?"
               styles={toInputBoxStyles}
               fetchDetails={true}
+              returnKeyType={"search"}
+              minLength={2}
+              onPress={(data, details = null) => {
+                dispatch({
+                  location: details.geometry.location,
+                  description: data.description,
+                });
+              }}
               enablePoweredByContainer={false}
               query={{
                 key: GOOGLE_MAPS_APIKEY,
