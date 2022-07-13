6b672d62c4dcf6f8c64163e24fa07aa3a26238eb
JuanFlores <juanflz7777@gmail.com>
2022-07-13 10:25:28 -0700

    rndered origin infomation on pin, created a stack navigator componenent or ride optionson bottom half of screen created navigatecard.js file

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,14 +3,30 @@ import React from "react";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import MapView from "react-native-maps";
+import { createStackNavigator } from "@react-navigation/native-stack";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
+        {/* RENDERS MAP ON TOP HALF OF SCREEN */}
         <Map />
       </View>
-      <View style={tw`h-1/2`}></View>
+
+      <View style={tw`h-1/2`}>
+        {/* RENDERS RIDE OPTIONS ON BOTTOM HALF OF SCREEN USING REACT-NAVIGATION  */}
+        <Stack.Navigator>
+          {/* CREATING A STACK OF SCREENS CALLED NAVIGATE CARD */}
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
