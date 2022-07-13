24b8287648c0b56fcd8d7158947b9886cbe9a0f3
JuanFlores <juanflz7777@gmail.com>
2022-07-13 12:36:01 -0700

    created and imported navigateCard component to mapscreen comp

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,7 +4,7 @@ import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import MapView from "react-native-maps";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigateCard } from "../components/NavigateCard";
+import NavigateCard from "../components/NavigateCard";
 
 const MapScreen = () => {
   const stack = createNativeStackNavigator();
